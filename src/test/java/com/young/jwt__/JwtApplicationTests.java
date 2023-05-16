96438b84434f62c73163f99be1dbed382ef8c5dd
hye-0000 <hye_0000@naver.com>
2023-05-16 14:42:27 +0900

    init

---
+++ src/test/java/com/young/jwt__/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.young.jwt__;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
